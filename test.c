08e04c2faeb5096f4dde0095131a0833718b9371
Adaías Magdiel <adaiasmagdiell@gmail.com>
2025-02-01 20:56:15 -0300

    tests: Add test for append

--- test.c
+++ test.c
@@ -24,9 +24,25 @@ void test_free() {
   ok("test_free");
 }
 
+void test_append() {
+  sb_free(&sb);
+  assert(sb_init(&sb) == 0);
+
+  sb_append(&sb, "Hello");
+  assert(strcmp("Hello", sb.data) == 0);
+
+  sb_append(&sb, ", ");
+  sb_append(&sb, "World");
+  sb_append(&sb, "!");
+  assert(strcmp("Hello, World!", sb.data) == 0);
+
+  ok("test_append");
+}
+
 int main() {
   test_init();
   test_free();
+  test_append();
 
   return 0;
 }
