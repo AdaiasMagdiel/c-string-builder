9e5deb374713d4d11308215030793499cc11bb07
Adaías Magdiel <adaiasmagdiell@gmail.com>
2025-02-03 10:34:31 -0300

    feat: add sb_replace test

--- test.c
+++ test.c
@@ -3,29 +3,50 @@
 #define MGDL_SB_IMPLEMENTATION
 #include "string_builder.h"
 
-StringBuilder sb;
-
 void ok(const char *test) { printf("\x1b[32m[PASS]\x1b[0m %s\n", test); }
 
 void test_init() {
+  StringBuilder sb;
+
   assert(sb_init(&sb) == 0);
   assert(sb.capacity == SB_DEFAULT_CAPACITY);
   assert(sb.length == 0);
 
+  sb_free(&sb);
   ok("test_init");
 }
 
 void test_free() {
+  StringBuilder sb;
+  assert(sb_init(&sb) == 0);
+
+  sb_append(&sb, "Some content");
+
   sb_free(&sb);
+
   assert(sb.data == NULL);
   assert(sb.capacity == SB_DEFAULT_CAPACITY);
   assert(sb.length == 0);
 
+  sb_free(&sb);
   ok("test_free");
 }
 
-void test_append() {
+void test_replace() {
+  StringBuilder sb;
+  sb_init(&sb);
+
+  sb_append(&sb, "Hello, World!");
+  sb_replace(&sb, "World", "C");
+
+  assert(strcmp(sb.data, "Hello, C!") == 0);
+
   sb_free(&sb);
+  ok("test_replace");
+}
+
+void test_append() {
+  StringBuilder sb;
   assert(sb_init(&sb) == 0);
 
   sb_append(&sb, "Hello");
@@ -36,6 +57,7 @@ void test_append() {
   sb_append(&sb, "!");
   assert(strcmp("Hello, World!", sb.data) == 0);
 
+  sb_free(&sb);
   ok("test_append");
 }
 
